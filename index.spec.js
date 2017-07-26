1fa8d7e34b09c7167598aa5595c4719e5ed618ae
Michael Novotny <manovotny@gmail.com>
2017-07-26 16:36:15 -0500

    Adds class name generator.

---
+++ index.spec.js
@@ -0,0 +1,22 @@
+const Chance = require('chance');
+
+const className = require('./index');
+
+describe('index.js', () => {
+    const chance = new Chance();
+
+    beforeEach(() => {
+        chance.mixin({
+            className
+        });
+    });
+
+    for (let i = 0; i < 100; i++) {
+        it('should begin with an underscore, a hyphen, or a letter, followed by any number of hyphens, underscores, letters, or numbers', () => {
+            const result = chance.className();
+            const regex = new RegExp(/^[-_a-zA-Z]{1}[-_a-zA-Z0-9]*$/, 'g');
+
+            expect(regex.test(result)).toBeTruthy();
+        });
+    }
+});
